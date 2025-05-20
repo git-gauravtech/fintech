4b2b83c5dca0b9e3887644b8ddfe21b1c7ec027e
git-gauravtech <gauravthe7841@gmail.com>
2025-05-20 09:34:44 +0530

    push it

--- src/Database/UserSession.java
+++ src/Database/UserSession.java
@@ -1,14 +1,5 @@
 package Database;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Lenovo
- */
 public class UserSession {
     public static int userId;
 }
--- src/personalfinancemanagement/PersonalFinanceManagement.java
+++ src/personalfinancemanagement/PersonalFinanceManagement.java
@@ -1,26 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package personalfinancemanagement;
 import javax.swing.JFrame;
 import Login.Login;
 
-/**
- *
- * @author hp
- */
+
 public class PersonalFinanceManagement {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         Login LoginFrame = new Login();
         LoginFrame.setVisible(true);  
         LoginFrame.pack();
-//        LoginFrame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Makes it full screen
         LoginFrame.setLocationRelativeTo(null); 
     }
     
